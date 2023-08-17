3ee55d414abc704c875f2f15b09885017f91db9b
decfinla <decfinla@uwaterloo.ca>
2023-08-16 22:41:54 -0400

    Projects section complete

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import WebScrape from "../assets/WebScraper.png";
 import SnackTrack from "../assets/SnackTrack.png";
 import Barbot from "../assets/BarBot.jpg";
 import Keychain from "../assets/KeychainAssembly.png";
-import Sudoku from "../assets/SudokuSolver.png";
+import Sudoku from "../assets/SudokuSolver1.png";
 import TronRobot from "../assets/TronDaysRobot.jpg";
 
 const Projects = () => {
@@ -25,20 +25,29 @@ const Projects = () => {
           {/* Grid Item 1*/}
           <div
             style={{ backgroundImage: `url(${Barbot})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/*Hover effects*/}
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                The Barbot
-              </span>
-              <div className="pt-8 text-center">
+              <div className="text-center">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  The BarBot
+                </span>
+              </div>
+              <div className="text-center">
+                <p className="px-4 py-2">
+                  The BarBot is an autonomous drink delivery robot built using
+                  Lego EV3 Mindstorm hardware, and programmed with RobotC to
+                  deliver drinks on a custom grid system.
+                </p>
+              </div>
+              <div className="text-center">
                 <a
                   href="https://github.com/DylanFinlay/BarBot"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:opacity-50">
                     GitHub
                   </button>
                 </a>
@@ -49,20 +58,29 @@ const Projects = () => {
           {/* Grid Item 2*/}
           <div
             style={{ backgroundImage: `url(${WebScrape})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/*Hover effects*/}
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Web Scraping API
-              </span>
-              <div className="pt-8 text-center">
+              <div className="text-center">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  Web Scraping API
+                </span>
+              </div>
+              <div className="text-center">
+                <p className="px-4 py-2">
+                  Python Web Scraper built to grab OG tags from thousands of
+                  websites, implemented as an API with Flask, and showcased
+                  through an intricate React Web App.
+                </p>
+              </div>
+              <div className="text-center">
                 <a
                   href="https://github.com/DylanFinlay/OpenGraph-Web-Scraper"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:opacity-50">
                     GitHub
                   </button>
                 </a>
@@ -71,8 +89,8 @@ const Projects = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    View Site
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:opacity-50">
+                    Site
                   </button>
                 </a>
               </div>
@@ -82,20 +100,29 @@ const Projects = () => {
           {/* Grid Item 3*/}
           <div
             style={{ backgroundImage: `url(${SnackTrack})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/*Hover effects*/}
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Snack Track
-              </span>
-              <div className="pt-8 text-center">
+              <div className="text-center">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  Snack Track
+                </span>
+              </div>
+              <div className="text-center">
+                <p className="px-4 py-2">
+                  Collaborated in a team of 5 to create a React App with a clean
+                  UI, that allows users to search for and access detailed
+                  nutritional information about various foods.
+                </p>
+              </div>
+              <div className="text-center">
                 <a
                   href="https://github.com/DylanFinlay/Snack-track"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:opacity-50">
                     GitHub
                   </button>
                 </a>
@@ -104,8 +131,8 @@ const Projects = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    View Site
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:opacity-50">
+                    Site
                   </button>
                 </a>
               </div>
@@ -115,20 +142,29 @@ const Projects = () => {
           {/* Grid Item 4*/}
           <div
             style={{ backgroundImage: `url(${Sudoku})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/*Hover effects*/}
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Sudoku Solver Game
-              </span>
-              <div className="pt-8 text-center">
+              <div className="text-center">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  Sudoku Solver
+                </span>
+              </div>
+              <div className="text-center">
+                <p className="px-4 py-2">
+                  Developed a back-tracking algorithm capable of solving any
+                  sudoku board, and further implemented a full sudoku game with
+                  a PyGame graphical user interface.
+                </p>
+              </div>
+              <div className="text-center">
                 <a
                   href="https://github.com/DylanFinlay/Sudoku-Solver-Game"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:opacity-50">
                     GitHub
                   </button>
                 </a>
@@ -139,26 +175,44 @@ const Projects = () => {
           {/* Grid Item 5*/}
           <div
             style={{ backgroundImage: `url(${TronRobot})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/*Hover effects*/}
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Mobile 3-Axis Robotic Arm
-              </span>
+              <div className="text-center">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  Mobile 3-Axis Robotic Arm
+                </span>
+              </div>
+              <div className="text-center">
+                <p className="px-4 py-2">
+                  Awarded first place in a University of Waterloo competition by
+                  designing a fully mobile 3-Axis Robotic Arm. The arm is
+                  controlled remotely to pick up and move small items.
+                </p>
+              </div>
             </div>
           </div>
 
           {/* Grid Item 6*/}
           <div
             style={{ backgroundImage: `url(${Keychain})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/*Hover effects*/}
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Custom Keychain Production
-              </span>
+              <div className="text-center">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  Custom Keychain Assembly
+                </span>
+              </div>
+              <div className="text-center">
+                <p className="px-4 py-2">
+                  Modelled, designed and produces a custom keychain. Seven
+                  individual pieces 3D Modelled precisely with SolidWorks, and
+                  further hand crafted in the shop.
+                </p>
+              </div>
             </div>
           </div>
         </div>
