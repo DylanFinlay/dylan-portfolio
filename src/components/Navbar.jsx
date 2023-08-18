f7e060913d1e974ca57387bc1cf4bcbd43b149b7
decfinla <decfinla@uwaterloo.ca>
2023-08-17 23:47:31 -0400

    Social Icons for mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,69 +47,115 @@ const Navbar = ({ nav, handleClick }) => {
       </div>
 
       {/*Mobile Menu*/}
-      <ul
+      <div
         className={
           !nav
             ? "hidden"
             : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className="py-6 text-4xl">
-          <Link
-            onClick={handleClick}
-            to="home"
-            smooth={true}
-            offset={-90}
-            duration={500}
-          >
-            Home
-          </Link>
-        </li>
-        <li className="py-6 text-4xl">
-          <Link
-            onClick={handleClick}
-            to="about"
-            smooth={true}
-            offset={-90}
-            duration={500}
-          >
-            About
-          </Link>
-        </li>
-        <li className="py-6 text-4xl">
-          <Link
-            onClick={handleClick}
-            to="projects"
-            smooth={true}
-            offset={-80}
-            duration={500}
-          >
-            Projects
-          </Link>
-        </li>
-        <li className="py-6 text-4xl">
-          <Link
-            onClick={handleClick}
-            to="skills"
-            smooth={true}
-            offset={-70}
-            duration={500}
-          >
-            Skills
-          </Link>
-        </li>
-        <li className="py-6 text-4xl">
-          <Link
-            onClick={handleClick}
-            to="contact"
-            smooth={true}
-            offset={-50}
-            duration={500}
-          >
-            Contact
-          </Link>
-        </li>
-      </ul>
+        <ul className="text-center">
+          <li className="py-6 text-4xl">
+            <Link
+              onClick={handleClick}
+              to="home"
+              smooth={true}
+              offset={-90}
+              duration={500}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link
+              onClick={handleClick}
+              to="about"
+              smooth={true}
+              offset={-90}
+              duration={500}
+            >
+              About
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link
+              onClick={handleClick}
+              to="projects"
+              smooth={true}
+              offset={-80}
+              duration={500}
+            >
+              Projects
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link
+              onClick={handleClick}
+              to="skills"
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              Skills
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link
+              onClick={handleClick}
+              to="contact"
+              smooth={true}
+              offset={-50}
+              duration={500}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+        {/* Social Icons for Mobile Menu */}
+        <div className="pt-20">
+          <p className="uppercase tracking-widest text-pink-600 text-center text-2xl">
+            Let&#39;s Connect
+          </p>
+          <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+            <a
+              href="https://www.linkedin.com/in/dylanfinlay33/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                <FaLinkedin size={30} />
+              </div>
+            </a>
+            <a
+              href="https://github.com/DylanFinlay"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                <FaGithub size={30} />
+              </div>
+            </a>
+            <a
+              href="mailto:dylan.finlay33@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                <HiOutlineMail size={30} />
+              </div>
+            </a>
+            <a
+              href="https://drive.google.com/file/d/1WqxA7fBbg-AEsLY0B1zxJjhM1mKJiZGg/view?usp=drive_link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                <BsFillPersonLinesFill size={30} />
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
 
       {/*Social Icons*/}
       <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
