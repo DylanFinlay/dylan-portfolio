b7be43e3165c345d01e682029782718e63e4e11c
Dylan Finlay <dylan.finlay33@gmail.com>
2024-11-01 14:37:42 -0400

    Added both hackathon projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,6 +8,8 @@ import Sudoku from "../assets/SudokuSolver1.png";
 import TronRobot from "../assets/TronDaysRobot.jpg";
 import MachineVision from "../assets/machinevision.PNG";
 import TempSensor from "../assets/TempSensor.jpg";
+import LogAnalyzer from "../assets/log_analyzer.png";
+import SolaceChatbot from "../assets/solaceChatbot.png";
 
 const Projects = ({ nav }) => {
   return (
@@ -101,6 +103,82 @@ const Projects = ({ nav }) => {
 
           {/* Grid Item 3*/}
           <div
+            style={{ backgroundImage: `url(${SolaceChatbot})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/*Hover effects*/}
+            <div className="opacity-0 group-hover:opacity-100">
+              <div className="text-center">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  Docs Dynamic Chatbot
+                </span>
+              </div>
+              <div className="text-center">
+                <p className="px-2 py-2">
+                  First place Solace hackathon project. Developed a dynamic
+                  chatbot custom-trained on the user's current page. Leveraged
+                  Anthropic's Claude-3-Sonnet LLM to generate quick responses.
+                </p>
+              </div>
+              <div className="text-center">
+                <a
+                  href={
+                    nav
+                      ? undefined
+                      : "https://github.com/DylanFinlay/SuperChargedChatBot"
+                  }
+                  onClick={nav ? (e) => e.preventDefault() : undefined}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    GitHub
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid Item 4*/}
+          <div
+            style={{ backgroundImage: `url(${LogAnalyzer})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/*Hover effects*/}
+            <div className="opacity-0 group-hover:opacity-100">
+              <div className="text-center">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  AI Workflow Analyzer
+                </span>
+              </div>
+              <div className="text-center">
+                <p className="px-2 py-2">
+                  Created an AI-Enhanced Github Actions Workflow Analyzer that
+                  uses RAG to reference workflow logs and source code, to then
+                  explain any previous failures and present possible solutions.
+                </p>
+              </div>
+              <div className="text-center">
+                <a
+                  href={
+                    nav
+                      ? undefined
+                      : "https://github.com/DylanFinlay/ai-workflow-analysis"
+                  }
+                  onClick={nav ? (e) => e.preventDefault() : undefined}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    GitHub
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid Item 5*/}
+          <div
             style={{ backgroundImage: `url(${TempSensor})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
@@ -137,7 +215,7 @@ const Projects = ({ nav }) => {
             </div>
           </div>
 
-          {/* Grid Item 4*/}
+          {/* Grid Item 6*/}
           <div
             style={{ backgroundImage: `url(${WebScrape})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
@@ -189,7 +267,7 @@ const Projects = ({ nav }) => {
             </div>
           </div>
 
-          {/* Grid Item 5*/}
+          {/* Grid Item 7*/}
           <div
             style={{ backgroundImage: `url(${SnackTrack})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
@@ -237,7 +315,7 @@ const Projects = ({ nav }) => {
             </div>
           </div>
 
-          {/* Grid Item 6*/}
+          {/* Grid Item 8*/}
           <div
             style={{ backgroundImage: `url(${Sudoku})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
@@ -275,7 +353,7 @@ const Projects = ({ nav }) => {
             </div>
           </div>
 
-          {/* Grid Item 7*/}
+          {/* Grid Item 9*/}
           <div
             style={{ backgroundImage: `url(${TronRobot})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
@@ -297,7 +375,7 @@ const Projects = ({ nav }) => {
             </div>
           </div>
 
-          {/* Grid Item 8*/}
+          {/* Grid Item 10*/}
           <div
             style={{ backgroundImage: `url(${Keychain})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
